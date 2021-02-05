9565474f78c83460ea2ebc8375672138fcae90fe
Kippi00 <caronicholas89813@gmail.com>
2021-02-05 02:33:35 -0500

    Get settings on startup if they do not yet exist

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,9 @@ module.exports = async client => {
   // Log that the bot is online.
   client.logger.log(`${client.user.tag}, ready to serve ${client.users.cache.size} users in ${client.guilds.cache.size} servers.`, "ready");
 
+  // If default settings do not exist in Enmap yetpm, create them.
+  await client.getSettings();
+
   // Make the bot "play the game" which is the help command with default prefix.
   client.user.setActivity(`${client.settings.get("default").prefix}help`, {type: "PLAYING"});
-  client.user.setStatus("dnd"); // Testing.
 };
