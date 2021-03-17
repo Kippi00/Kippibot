79b180bd6eb6eb7fadea113e2fd7578c78b9472a
Kippi00 <caronicholas89813@gmail.com>
2021-03-17 04:24:40 -0400

    update stats.js
    now displays current time

--- commands/discord/stats.js
+++ commands/discord/stats.js
@@ -3,6 +3,7 @@ const moment = require("moment");
 require("moment-duration-format");
 
 exports.run = (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  const currentTime = moment().format("LLLL Z")
   const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
   message.channel.send(`= STATISTICS =
 • Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
@@ -11,7 +12,8 @@ exports.run = (client, message, args, level) => { // eslint-disable-line no-unus
 • Servers    :: ${client.guilds.cache.size.toLocaleString()}
 • Channels   :: ${client.channels.cache.size.toLocaleString()}
 • Discord.js :: v${version}
-• Node       :: ${process.version}`, {code: "asciidoc"});
+• Node       :: ${process.version}
+• Time       :: ${currentTime}`, {code: "asciidoc"});
 };
 
 exports.conf = {
